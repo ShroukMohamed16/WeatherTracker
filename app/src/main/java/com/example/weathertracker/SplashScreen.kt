ce3eda60cf5e2da501d33451c176e9ee1a1650b3
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2023-06-24 19:10:04 +0300

    Splash Screen

--- app/src/main/java/com/example/weathertracker/SplashScreen.kt
+++ app/src/main/java/com/example/weathertracker/SplashScreen.kt
@@ -1,11 +1,23 @@
 package com.example.weathertracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this@SplashScreen, "Welcome", Toast.LENGTH_SHORT).show()
+
+        }, 5000)
     }
 }
