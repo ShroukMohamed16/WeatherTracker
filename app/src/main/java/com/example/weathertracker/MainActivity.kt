dba708392a1ea6a8b6466f14b8fe85d061a719eb
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2023-06-25 11:45:26 +0300

    UI Part_1

--- app/src/main/java/com/example/weathertracker/MainActivity.kt
+++ app/src/main/java/com/example/weathertracker/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.weathertracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
+        val  actionBar = supportActionBar
+        if(actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.menu_icon)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setDisplayShowHomeEnabled(true)
+        }
+        val navController =  Navigation.findNavController(this,R.id.nav_host)
+        NavigationUI.setupWithNavController(navigationView , navController)
+
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun initUI() {
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
     }
 }
